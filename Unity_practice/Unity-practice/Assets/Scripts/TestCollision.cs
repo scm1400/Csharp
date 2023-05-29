e190bd8f4c0a8b33af9d499b24683fb6095adfc8
D.심은철 <scm1400@gmail.com>
2023-05-29 22:53:28 +0900

    카메라에서 클릭 지점으로 ray 쏘기

--- Unity_practice/Unity-practice/Assets/Scripts/TestCollision.cs
+++ Unity_practice/Unity-practice/Assets/Scripts/TestCollision.cs
@@ -26,21 +26,52 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        
-        Vector3 look = transform.TransformDirection(Vector3.forward); // world to local
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        // Debug.Log(Input.mousePosition);
 
-        //  여러개를 관통할 경우 관통한 모든 오브젝트를 인식함 //
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
 
-        foreach (RaycastHit hit in hits)
+        // 카메라에서 클릭지점으로 ray 발사 ( 유니티에 구현된 기능 활용 )
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log($"Raycast {hit.collider.gameObject.name}");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
         }
-
         
-        //  여러개를 관통할 경우 처음 관통한 오브젝트를 인식함 //
+        // 카메라에서 클릭지점으로 ray 발사 ( 상세구현 )
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+        //         Camera.main.nearClipPlane));
+        //     Vector3 dir = mousePos - Camera.main.transform.position;
+        //     dir = dir.normalized;
+        //     Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+        //     RaycastHit hit;
+        //     if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+        //     {
+        //         Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //     }
+        // }
+
+
+        //  raycast에 사용하는 공통 코드 //
+        // Vector3 look = transform.TransformDirection(Vector3.forward); // local to world
+        // Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        // RaycastAll 여러개를 관통할 경우 관통한 모든 오브젝트를 인식함 //
+        // RaycastHit[] hits;
+        // hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        //
+        // foreach (RaycastHit hit in hits)
+        // {
+        //     Debug.Log($"Raycast {hit.collider.gameObject.name}");
+        // }
+
+        //  Raycast 여러개를 관통할 경우 처음 관통한 오브젝트를 인식함 //
         // RaycastHit hit;
         // if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10)) ;
         // {
